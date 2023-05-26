f3064524ec661c590d2a0e394111b39a18c27ad5
aleena <aleenababy22333@gmail.com>
2023-05-26 20:25:27 +0530

    Initial commit

---
+++ FacialRecognition/03_face_recognition.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+import os 
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('Trainer_face_recognition/trainer.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Initialize id counter
+id = 0
+
+# Read names from file
+with open('names.txt', 'r') as file:
+    names = file.read().splitlines()
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)  # Set video width
+cam.set(4, 480)  # Set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
+
+while True:
+    ret, img = cam.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=2,
+        minSize=(int(minW), int(minH)),
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+        print(confidence)
+
+        # Check if confidence is less than 100
+        if confidence > 55 and id < len(names):
+            id = names[id]
+        else:
+            #print(names[id])
+            id = "unknown"
+
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('camera', img)
+
+    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
